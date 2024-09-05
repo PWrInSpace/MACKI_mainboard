35664b48417a5a8e59df482e31e4859821a421c3
kgliwinski <259282@student.pwr.edu.pl>
2024-09-05 03:07:13 +0200

    Second TEMP1075 sensor tested

--- src/components/ic_drivers/test/test_tmp1075_driver.c
+++ src/components/ic_drivers/test/test_tmp1075_driver.c
@@ -37,23 +37,53 @@ bool i2c_ic_send_data(uint8_t* data, size_t data_size, uint8_t device_address) {
   }
   i2c_driver_status_t status = i2c_driver_send_data(i2c_driver_instance, data,
                                                     data_size, device_address);
+  printf("status: %d\n", status);
   return status == I2C_DRIVER_OK;
 }
 
-tmp1075_driver_t tmp1075_driver = {
-    .address = 0b1001000,
-    ._send_receive_data = &i2c_ic_send_receive_data,
-    ._send_data = &i2c_ic_send_data,
-};
+typedef enum {
+  TMP1075_U6 = 0,
+  TMP1075_U7,
+  NUM_TMP1075_DRIVERS
+} tmp1075_driver_index_t;
 
-TEST_CASE("TMP1075 driver initialization", "[TMP1075_DRIVER]") {
+tmp1075_driver_t tmp1075_drivers[NUM_TMP1075_DRIVERS] = {
+    [TMP1075_U6] =
+        {
+            .address = 0b1001000,
+            ._send_receive_data = &i2c_ic_send_receive_data,
+            ._send_data = &i2c_ic_send_data,
+        },
+    // TODO(GLibus): Fix address test when correct sensor is connected
+    [TMP1075_U7] = {
+        .address = 0b1001001,
+        ._send_receive_data = &i2c_ic_send_receive_data,
+        ._send_data = &i2c_ic_send_data,
+    }};
+
+TEST_CASE("TMP1075 driver U6 initialization", "[TMP1075_DRIVER]") {
   init_i2c_driver();
-  TEST_ASSERT_TMP1075_DRIVER_OK(tmp1075_driver_init(&tmp1075_driver));
+
+  TEST_ASSERT_TMP1075_DRIVER_OK(
+      tmp1075_driver_init(&tmp1075_drivers[TMP1075_U6]));
+}
+TEST_CASE("TMP1075 driver U7 initialization", "[TMP1075_DRIVER]") {
+  TEST_ASSERT_TMP1075_DRIVER_OK(
+      tmp1075_driver_init(&tmp1075_drivers[TMP1075_U7]));
 }
 
-TEST_CASE("TMP1075 driver read device ID", "[TMP1075_DRIVER]") {
+TEST_CASE("TMP1075 driver U6 read device ID", "[TMP1075_DRIVER]") {
   uint16_t device_id;
   TEST_ASSERT_TMP1075_DRIVER_OK(
-      tmp1075_driver_read_device_id(&tmp1075_driver, &device_id));
+      tmp1075_driver_read_device_id(&tmp1075_drivers[TMP1075_U6], &device_id));
   TEST_ASSERT_EQUAL_HEX16(0x7500, device_id);
 }
+
+// TODO(Glibus): Fix this test when correct sensor
+//  TEST_CASE("TMP1075 driver U7 read device ID", "[TMP1075_DRIVER]") {
+//    uint16_t device_id;
+//    TEST_ASSERT_TMP1075_DRIVER_OK(
+//        tmp1075_driver_read_device_id(&tmp1075_drivers[TMP1075_U7],
+//        &device_id));
+//    TEST_ASSERT_EQUAL_HEX16(0x7500, device_id);
+//  }
