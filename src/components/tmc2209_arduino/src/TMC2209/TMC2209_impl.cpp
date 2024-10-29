e9e923fcb3c296a89e959fe87b65c521ed9f4c3c
JSiuda1 <jakkub68@gmail.com>
2024-10-29 21:46:03 +0100

    Set hold current to 100%

--- src/components/tmc2209_arduino/src/TMC2209/TMC2209_impl.cpp
+++ src/components/tmc2209_arduino/src/TMC2209/TMC2209_impl.cpp
@@ -59,6 +59,7 @@ void tmc2209_c_init(stepper_motor_instances_t instance) {
   stepper_drivers[instance].setRunCurrent(RUN_CURRENT_PERCENT);
   stepper_drivers[instance].enableCoolStep();
   stepper_drivers[instance].enable();
+  stepper_drivers[instance].setHoldCurrent(100);
 }
 
 void tmc2209_c_enable(stepper_motor_instances_t instance) {
